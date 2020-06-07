0b7c36033d6230b156b2174f421a95de4da90f6f
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2020-06-08 01:06:46 +0800

    Create p21.cpp

---
+++ p21.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define author_by_CutieRabbit ios::sync_with_stdio(false); cin.tie(0);
+using namespace std;
+
+int main() { author_by_CutieRabbit
+    int a, b;
+    while(cin >> a >> b) {
+        cout << __gcd(a, b) << endl;
+    }
+}
