deabe8d9f2fde720bcaa27e9ca3e18475beb7e69
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2020-06-08 01:35:02 +0800

    Create p14.cpp

---
+++ p14.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m, d;
+    cin >> m >> d;
+    switch(m){
+        case 1:d<21?cout<<"Capricorn"<<endl:cout<<"Aquarius"<<endl;break;
+        case 2:d<19?cout<<"Aquarius"<<endl:cout<<"Pisces"<<endl;break;
+        case 3:d<21?cout<<"Pisces"<<endl:cout<<"Aries"<<endl;break;
+        case 4:d<21?cout<<"Aries"<<endl:cout<<"Taurus"<<endl;break;
+        case 5:d<22?cout<<"Taurus"<<endl:cout<<"Gemini"<<endl;break;
+        case 6:d<22?cout<<"Gemini"<<endl:cout<<"Cancer"<<endl;break;
+        case 7:d<23?cout<<"Cancer"<<endl:cout<<"Leo"<<endl;break;
+        case 8:d<24?cout<<"Leo"<<endl:cout<<"Virgo"<<endl;break;
+        case 9:d<24?cout<<"Virgo"<<endl:cout<<"Libra"<<endl;break;
+        case 10:d<24?cout<<"Libra"<<endl:cout<<"Scorpio"<<endl;break;
+        case 11:d<23?cout<<"Scorpio"<<endl:cout<<"Sagittarius"<<endl;break;
+        case 12:d<22?cout<<"Sagittarius"<<endl:cout<<"Capricorn"<<endl;break;
+    }
+    return 0;
+}
