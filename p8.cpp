1131687935f40c03d09c38e443fa5695ae5be9bb
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2020-06-08 00:38:04 +0800

    Create p8.cpp

---
+++ p8.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define author_by_CutieRabbit ios::sync_with_stdio(false); cin.tie(0);
+using namespace std;
+
+int main() { author_by_CutieRabbit
+    int a, b;
+    while(cin >> a >> b){
+        cout << a-(b/2-a) << endl;
+        cout <<(b/2-a) << endl;
+    }
+}
